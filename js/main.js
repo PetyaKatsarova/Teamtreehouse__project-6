0763b10e1f0ca82b24205910080e7927bdf8ac47
pskpetya <pskpetya@gmail.com>
2018-03-09 15:51:30 +0400

    creating project-6
    teamtreehouse, adding my files

---
+++ js/main.js
@@ -0,0 +1,27 @@
+// parts of the text will highlight when pronounced on the audio
+
+const video = document.querySelector('video');
+let span = document.querySelectorAll('.captions span');
+
+video.addEventListener("timeupdate",()=>{
+    for(let i=0; i<span.length; i++){
+        let dataStart =span[i].getAttribute('data-start');
+        let dataEnd = span[i].getAttribute('data-end');
+        let currentTime = video.currentTime;
+
+        if(currentTime > dataStart && currentTime < dataEnd){
+             span[i].style.color= 'orange';
+        } else {
+            span[i].style.color='black';
+        }
+    }
+});
+
+// exceed expectations: when clicking on any sentence, video player jumps to that sentence
+
+for( let i = 0; i<span.length; i++){
+span[i].addEventListener('click', (e)=>{
+  video.currentTime = e.target.getAttribute('data-start');
+  video.play();
+});
+}
